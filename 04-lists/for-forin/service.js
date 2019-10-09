eaa5903bd2899e11ddf797fa699ef95b793c2b56
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2019-10-08 22:33:51 -0300

    Update

---
+++ 04-lists/for-forin/service.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const URL = `https://swapi.co/api/people`;
+
+async function obterPessoas (nome){
+    const url = `${URL}/?search=${nome}&format=json`
+    const response = await axios.get(url)
+    
+    return response.data
+}
+
+// obterPessoas('r2')
+// .then(function (response){
+//     console.log(('resultado', response));
+// })
+// .catch(function (error){
+//     console.error('Erro', error);
+    
+// }) 
+
+module.exports = {
+    obterPessoas
+}
+
