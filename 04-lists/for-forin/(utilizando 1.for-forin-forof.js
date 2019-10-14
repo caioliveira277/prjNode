47cd2c2b28f2586e3ab12f6eb134cf9392305652
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2019-10-14 09:22:46 -0300

    update

--- 04-lists/for-forin/(utilizando 1.for-forin-forof.js
+++ 04-lists/for-forin/(utilizando 1.for-forin-forof.js
@@ -1,27 +1,35 @@
 const service = require('./service') // ./ para modulos que eu exportei
 
+
+
 async function main(){
     try{
+        console.time('start');
         const result = await service.obterPessoas('a');
-        const names = [];
+        console.log(result);
         
-        for(let i = 0; i <= result.results.lenght - 1; i++){
-            const pessoa = result.results[i];
-            names.push(pessoa.name);
-        }
-
-        for(let i in result.results){ // para este caso foi o mais performatico
-            const pessoa = result.results[i]
-            names.push(pessoa.name);
-        }
-
-        for(pessoa of result.results){
-            names.push(pessoa.name)
-        }
+        console.timeEnd('start');
+        // const names = [];
+        
+        // for(let i = 0; i <= result.results.lenght - 1; i++){
+        //     const pessoa = result.results[i];
+        //     names.push(pessoa.name);
+        // }
 
 
+        // for(let i in result.results){ // para este caso foi o mais performatico
+        //     const pessoa = result.results[i]
+        //     names.push(pessoa.name);
+        // }
+       
 
-        console.log('names', names);
+        // for(pessoa of result.results){
+        //     names.push(pessoa.name)
+        // }
+
+
+
+        // console.log('names', names);
         
 
     }
